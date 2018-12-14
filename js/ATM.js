99eea686f3e5b5af96bd5ebad55e9a23e6251079
Justin Harthcock <harthcockbusiness@gmail.com>
2018-12-14 14:29:24 -0600

    Updated ATM

--- js/ATM.js
+++ js/ATM.js
@@ -1,10 +1,10 @@
 var nonChar = '';
 var Users = {
     User1 :{
+        userName: "Justin",
         userBalance: 100,
         cardNumber: 4567,
         pinNum: 7654,
-        userName: "Justin"
     },
     User2 : {
         userBalance: 100000000000,
@@ -14,6 +14,12 @@ var Users = {
     }
 
 }
+// function accountInfo(){
+//     display.innerHTML = '<h4>' + Users.User1 + '</h4>';
+    
+// }
+
+
 var display = document.getElementById("display");
 // Makes the output screen blank
 function start(){
@@ -32,6 +38,7 @@ function start(){
         change[4].setAttribute("onclick", "")
         change[5].setAttribute("onclick", "")
 }
+
 
 function CardNum(){
     // targets the input created in the 'start()' function
@@ -53,39 +60,48 @@ function CardNum(){
         display.innerHTML = "Not valid try again."
     }
 }
+
+
 function pin(){
     // Targets the input created in "CardNum()'s" if statement.
     var cardNum = document.getElementById("cardNum");
     //Cognates the the input from the user into numbers so that the value can be read.
-    if(Number(cardNum.value) === Users.User1.pinNum|| User2.pinNum){
+    if(Number(cardNum.value) === Users.User1.pinNum){
         nonChar = "";
 
         //Displays this after the if statment is true.
         //This function makes all of the words line up with all the buttons. Example top left button is b1, now everything under class b1 will be in the same space no matter what page we are on.
-        display.innerHTML ="<h4>Would you like to : </h4><br>"+ '<h4 class="b1">Withdrawl</h4> <h4 class="b4">Account Info</h4><h4 class="b2">Deposit</h4><h4 class= "b5">Go to Savings</h4><h4  class="tab b3"  >Fast Cash</h4><h4 class="b6">Exit</h4 ></div>'
+        display.innerHTML ="<h4>Would you like to : </h4><br>"+ '<h4 class="b1">Withdrawl</h4> <h4 class="b4">Account Info</h4><h4 class="b2">Deposit</h4><h4 class= "b5"></h4><h4  class="tab b3"  >Fast Cash</h4><h4 class="b6">Exit</h4 ></div>';
         //These are the buttons on the "Main Page."
         var change = document.getElementsByTagName('button');
-        change[0].setAttribute("onclick", "withdraw()")
-        change[1].setAttribute("onclick", "deposit()")
-        change[2].setAttribute("onclick", "fastCash()")
-        change[3].setAttribute("onclick", "accountInfo()")
-        change[4].setAttribute("onclick", "goToSavings()")
-        change[5].setAttribute("onclick", "exit()")
+        change[0].setAttribute("onclick", "withdraw()");
+        change[1].setAttribute("onclick", "deposit()");
+        change[2].setAttribute("onclick", "fastCash()");
+        change[3].setAttribute("onclick", "accountInfo()");
+        change[4].setAttribute("onclick", "goToSavings()");
+        change[5].setAttribute("onclick", "exit()");
+    }
+    else{
+        display.innerHTML = "Not valid try again."
     }
 }
+
+
 function Continue(){
-    display.innerHTML = '<h4>Would you like to : </h4><br>'+ '<h4 class="b1">Withdrawl</h4> <h4 class="b4">Account Info</h4><h4 class="b2">Deposit</h4><h4 class= "b5">Go to Savings</h4><h4  class="tab b3"  >Fast Cash</h4><h4 class="b6">Exit</h4 ></div>'
+    display.innerHTML = '<h4>Would you like to : </h4><br>'+ '<h4 class="b1">Withdrawl</h4> <h4 class="b4">Account Info</h4><h4 class="b2">Deposit</h4><h4 class= "b5">Go to Savings</h4><h4  class="cb3"  >Fast Cash</h4><h4 class="b6">Exit</h4 ></div>';
         //These are the buttons on the "Main Page."
         var change = document.getElementsByTagName('button');
-        change[0].setAttribute("onclick", "withdraw()")
-        change[1].setAttribute("onclick", "deposit()")
-        change[2].setAttribute("onclick", "fastCash()")
-        change[3].setAttribute("onclick", "accountInfo()")
-        change[4].setAttribute("onclick", "goToSavings()")
-        change[5].setAttribute("onclick", "exit()")
+        change[0].setAttribute("onclick", "withdraw()");
+        change[1].setAttribute("onclick", "deposit()");
+        change[2].setAttribute("onclick", "fastCash()");
+        change[3].setAttribute("onclick", "accountInfo()");
+        change[4].setAttribute("onclick", "goToSavings()");
+        change[5].setAttribute("onclick", "exit()");
 }
+
+
 function fastCash(){
-    display.innerHTML = '<h4>What amount would you like to Withdraw out of Fast Cash?: </h4><br>'+ '<h4 class="b1">20</h4> <h4 class="b4">80</h4><h4 class="b2">40</h4><h4 class= "b5">100</h4><h4  class="tab b3"  >60</h4><h4 class="b6">120</h4 ></div>'
+    display.innerHTML = '<h4>What amount would you like to Withdraw out of Fast Cash?: </h4><br>'+ '<h4 class="FCb1">20</h4> <h4 class="FCb4">80</h4><h4 class="FCb2">40</h4><h4 class= "FCb5">100</h4><h4  class="FCb3"  >60</h4><h4 class="FCb6">120</h4 ></div>';
     var change = document.getElementsByTagName("button");
     change[0].setAttribute("onClick", "F20();");
     change[1].setAttribute("onClick", "F40();");
@@ -94,10 +110,12 @@ function fastCash(){
     change[4].setAttribute("onClick", "F100();");
     change[5].setAttribute("onClick", "F120();");
 }
+
+
 function F20(){
     var newbalance = Users.User1.userBalance - 20;
     Users.User1.userBalance = newbalance;
-    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>"
+    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>";
     var change = document.getElementsByTagName("button");
     change[0].setAttribute("onClick", ";");
     change[1].setAttribute("onClick", "fastCash();;");
@@ -106,10 +124,12 @@ function F20(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function F40(){
     var newbalance = Users.User1.userBalance - 40;
     Users.User1.userBalance = newbalance;
-    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>"
+    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>";
     var change = document.getElementsByTagName("button");
     change[0].setAttribute("onClick", ";");
     change[1].setAttribute("onClick", "fastCash();;");
@@ -118,10 +138,12 @@ function F40(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function F60(){
     var newbalance = Users.User1.userBalance - 60;
     Users.User1.userBalance = newbalance;
-    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>"
+    display.innerHTML = "<h4>Your current balance is: </h4>" + newbalance + "<h4>Would you like to make another transaction?</h4><h4 class='b2'>Yes</h4><h4 class= 'b5'>No</h4>";
     var change = document.getElementsByTagName("button");
     change[0].setAttribute("onClick", ";");
     change[1].setAttribute("onClick", "fastCash();;");
@@ -130,6 +152,8 @@ function F60(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function F80(){
     var newbalance = Users.User1.userBalance - 80;
     Users.User1.userBalance = newbalance;
@@ -142,6 +166,8 @@ function F80(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function F100(){
     var newbalance = Users.User1.userBalance - 100;
     Users.User1.userBalance = newbalance;
@@ -154,6 +180,8 @@ function F100(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function F120(){
     var newbalance = Users.User1.userBalance - 120;
     Users.User1.userBalance = newbalance;
@@ -166,11 +194,13 @@ function F120(){
     change[4].setAttribute("onClick", "Continue();");
     change[5].setAttribute("onClick", ";");
 }
+
+
 function withdraw(){
     display.innerHTML = '<h4>What amount would you like to Withdraw out: </h4><br>'+ '<input type="number" class="keyInput" id="cardNum">';
     nonChar = "";
     var change = document.getElementsByTagName("button");
-    change[20].setAttribute("onclick", "withdrawAmount();")
+    change[20].setAttribute("onclick", "withdrawAmount();");
     change[0].setAttribute("onClick", "withdrawAmount();");
     change[1].setAttribute("onClick", "withdrawAmount();");
     change[2].setAttribute("onClick", "withdrawAmount();");
@@ -178,6 +208,7 @@ function withdraw(){
     change[4].setAttribute("onClick", "withdrawAmount();");
     change[5].setAttribute("onClick", "withdrawAmount();");
 }
+
 
 //This function is for Withdrawal
 function withdrawAmount(){
@@ -193,8 +224,8 @@ function withdrawAmount(){
     change[4].setAttribute("onClick", ";");
     change[5].setAttribute("onClick", "Continue();");
     change[20].setAttribute("onclick", ";")
-
 }
+
 
 function deposit(){
     // var newbalance = Users.User1.userBalance + Number(amount.value);
@@ -212,6 +243,8 @@ function deposit(){
     change[5].setAttribute("onClick", ";");
     change[20].setAttribute("onclick", "depositAmount();")
 }
+
+
 function depositAmount(){
     var amount = document.getElementById("cardNum");
     var newbalance = Users.User1.userBalance + Number(amount.value);
@@ -227,25 +260,36 @@ function depositAmount(){
     change[20].setAttribute("onclick", ";")
 }
 
-function goToSavings(){
-    display.innerHTML = "<h4>Your name is: " + userName + "</h4><br> <h4> Your current balance is: " + newbalance + "</h4><br>"
+
+function accountInfo(){
+    display.innerHTML = "<h4>Your name is: " + Users.User1.userName + "</h4><br> <h4> Your current balance is: " + Users.User1.userBalance + "</h4><br>";
 }
+
+
 //Key-pad
 function clearPassword() {
-    clickedButton(nonChar="") 
+    clickedButton(nonChar="");
    };
+
+
    function deleteChar(){
-    clickedButton(nonChar = nonChar.substring(0,nonChar.length-1))
-   }; 
+    clickedButton(nonChar = nonChar.substring(0,nonChar.length-1));
+   };
+   
+
    function clickedButton(btn){
     var key = document.querySelector('.keyInput');
     key.value = btn;
     console.log(btn);
-   }; 
+   };
+   
+   
    function showNumPad(btn){
-    clickedButton(nonChar+=btn)
+    clickedButton(nonChar+=btn);
     console.log(btn);
-   }; 
+   };
+   
+   
 function exit(){
-    location.reload()
+    location.reload();
 }
